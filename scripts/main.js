f0ea1df58e9d34a3599828e3816407907f818de9
JonathanTestUser <jonnoxxcv@gmail.com>
2017-01-04 15:53:27 -0800

    Search Bar Complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,8 @@
 
-//Inserts herb values into an array.
-var database=[];
-					
+//want the database in the global scope to be accessable from other functions.
+var database=[]; 
+
+//The Herb object with all variables.					
 function Herb(name, effect1, val1, mag1, effect2, val2, mag2, effect3, val3, mag3, effect4, val4, mag4){
 	this.name = name;
 	this.effect1 = effect1;
@@ -18,58 +19,78 @@ function Herb(name, effect1, val1, mag1, effect2, val2, mag2, effect3, val3, mag
 	this.mag4 = mag4;
 }
 
+//The window on load function, automatically run, will compile a database from text.
 window.onload = function(){
-	var herb1 = new Herb ('Abecean Longfin', 
-						  'Weakness to Frost', 1.0, 2.0,
-						  'Fortify Sneak', 3.0, 4.0, 
-						  'Weakness to Poison', 5.0, 6.0,
-						  'Fortify Restoration', 7.0, 8.0);
 
-	var herb2 = new Herb ('Ancestor Moth Wing', 
-						  'Damage Stamina', 1.0, 1.0,
-						  'Fortify Conjuration', 1.0, 1.0, 
-						  'Damage Magicka Regen', 1.0, 1.0,
-						  'Fortify Enchanting', 1.0, 1.0);
-	database= database.concat([herb1],[herb2]);
-	//alert (database[1].name);
-	var file = 'data.txt';
+	//The source file to load
+	var file = 'data.csv';
+	
+	//make a new XMLHttpRequest
     var rawFile = new XMLHttpRequest();
+	
+	//Set response type.
 	rawFile.responseType = 'text';
+	
+	//GET the file data
     rawFile.open('GET', file, true);
+	
+	//wait until ready
     rawFile.onreadystatechange = function ()
     {
+		//All Done!
 		if(rawFile.readyState === rawFile.DONE && (rawFile.status === 200 || rawFile.status == 0))
 		{
-			var rawData=rawFile.responseText;
-			var firstSplit = rawData.split("\n");
-			var firstString = firstSplit[0];
-			var secondSplit = firstString.split(",");
-			alert (secondSplit[0]);
+			//Store response to raw Data.
+			var rawData = rawFile.responseText;
+			
+			//Split file into array by linebreaks, removing whitespace
+			var firstSplit = rawData.trim.split("\n");
+			
+			//Get length of array
+			var max = firstSplit.length;
+			
+			//Loop through the array
+			for (var i = 0; i < max; i++) { 
+			
+				//Get each ingredient as a string of values
+				var ingredientString = firstSplit[i];
+				
+				//split that string by commas, discarding whitespace
+				var secondSplit = ingredientString.trim.split(",");
+				
+				//check all fields are there...
+				if (secondSplit.length === 13){
+					
+					//add to database.
+					database= database.concat([new Herb(secondSplit[0],secondSplit[1],secondSplit[2],secondSplit[3],secondSplit[4],secondSplit[5],secondSplit[6],secondSplit[7],secondSplit[8],secondSplit[9],secondSplit[10],secondSplit[11],secondSplit[12])]);
+				}
+			}
 		}
     }
     rawFile.send(null);
 }
 	
 
-
+//Search functionality
 function searchHerbs() {
+	
+	//get herbName from search box
 	var herbName = document.getElementById("herbName").value;
+	herbName= herbName.trim;
+	
+	//convert into a regex string
+	var regExpHerb = regexp (herbName, i);
+	
+	//Get the Div where data will be shown.
 	var divField = document.getElementById('dataReturn');
+	
+	//while there is a first child
 	while(divField.firstChild){
+	//remove first child
     divField.removeChild(divField.firstChild);
-  }
+ }
 	
-	var herb = new Herb;
-	
-	switch (herbName){
-		case (herb1.name):
-			herb = herb1;
-		break;
-		case (herb2.name):
-			herb = herb2;
-		break;
-	}
-	
+	//create a table, insert a row of headers.
 	var table = document.createElement("table");
 	var tr = table.insertRow();
 	var th = document.createElement('th');
@@ -90,41 +111,47 @@ function searchHerbs() {
 	th.innerHTML = "Mag";
 	tr.appendChild(th);
 	
-	tr = table.insertRow();
-	td = tr.insertCell();
-	td.innerHTML =  herb.name;
-	td.setAttribute('rowspan', '4');
-	td = tr.insertCell();
-	td.innerHTML =  herb.effect1;
-	td = tr.insertCell();
-	td.innerHTML =  herb.val1;
-	td = tr.insertCell();
-	td.innerHTML =  herb.mag1;
-	
-	tr = table.insertRow();
-	td = tr.insertCell();
-	td.innerHTML =  herb.effect2;
-	td = tr.insertCell();
-	td.innerHTML =  herb.val2;
-	td = tr.insertCell();
-	td.innerHTML =  herb.mag2;
-	
-	tr = table.insertRow();
-	td = tr.insertCell();
-	td.innerHTML =  herb.effect3;
-	td = tr.insertCell();
-	td.innerHTML =  herb.val3;
-	td = tr.insertCell();
-	td.innerHTML =  herb.mag3;
-	
-	tr = table.insertRow();
-	td = tr.insertCell();
-	td.innerHTML =  herb.effect4;
-	td = tr.insertCell();
-	td.innerHTML =  herb.val4;
-	td = tr.insertCell();
-	td.innerHTML =  herb.mag4;
-
+	//loop through the database.
+	var databaseLength = database.length;
+	for (var i= 0; i < databaseLength; i++){
+		if (regExpHerb.test (database[i].name) || regExpHerb.test(database[i].effect1) || regExpHerb.test(database[i].effect2) || regExpHerb.test(database[i].effect3) || regExpHerb.test(database[i].effect4)){
+			
+			tr = table.insertRow();
+			td = tr.insertCell();
+			td.innerHTML =  database[i].name;
+			td.setAttribute('rowspan', '4');
+			td = tr.insertCell();
+			td.innerHTML =  database[i].effect1;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].val1;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].mag1;
+			
+			tr = table.insertRow();
+			td = tr.insertCell();
+			td.innerHTML =  database[i].effect2;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].val2;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].mag2;
+			
+			tr = table.insertRow();
+			td = tr.insertCell();
+			td.innerHTML =  database[i].effect3;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].val3;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].mag3;
+			
+			tr = table.insertRow();
+			td = tr.insertCell();
+			td.innerHTML =  database[i].effect4;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].val4;
+			td = tr.insertCell();
+			td.innerHTML =  database[i].mag4;
+		}
+	}
 	divField.appendChild(table);
 }
 	
