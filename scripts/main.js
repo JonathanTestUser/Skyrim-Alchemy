0af9ab712cde96cc9babad409c14f668e97d59b9
JonathanTestUser <jonnoxxcv@gmail.com>
2017-01-04 16:16:05 -0800

    syntax error

--- scripts/main.js
+++ scripts/main.js
@@ -69,6 +69,7 @@ window.onload = function(){
 					database= database.concat([new Herb(secondSplit[0],secondSplit[1],secondSplit[2],secondSplit[3],secondSplit[4],secondSplit[5],secondSplit[6],secondSplit[7],secondSplit[8],secondSplit[9],secondSplit[10],secondSplit[11],secondSplit[12])]);
 				}
 			}
+		}
 		
     }
     rawFile.send(null);
